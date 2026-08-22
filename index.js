c55c1b0859d096b5cdaa6ea75abc4f382883c552
Ranjan Kumar <imranjanyadav2001@gmail.com>
2026-08-22 16:12:19 +0530

    insert values in db and create /project route

--- index.js
+++ index.js
@@ -23,9 +23,58 @@ connection.connect((err) => {
         console.log("Database connection failed:", err);
         return;
     }
-
     console.log("MySQL connected!");
 });
+
+//inserting values in table.
+
+let q = `INSERT INTO projects
+(name, description, technologies, github)
+VALUES ?`
+
+const projects = [
+    [
+        "Portfolio Website",
+        "A personal portfolio website to showcase my skills and projects.",
+        "HTML, CSS, JavaScript, Node.js, Express.js, EJS, MySQL",
+        null
+    ],
+    [
+        "Student Management System",
+        "A web application for managing student records.",
+        "Node.js, Express.js, EJS, MySQL",
+        null
+    ],
+    [
+        "Crime Reporting Website",
+        "A web application for submitting and managing crime reports.",
+        "Python, Flask, HTML, CSS, JavaScript, MySQL",
+        null
+    ]
+];
+
+// try{
+// connection.query(q,[projects],(err,result)=>{
+//     if(err) throw err;
+//     console.log(result);
+// });
+// }catch(err){
+//   console.log(err);
+// }
+
+app.get("/projects",(req,res)=>{
+  let q = `SELECT * FROM projects`;
+  try{
+    connection.query(q,(err,result)=>{
+    if(err)throw err;
+   let projects = result;
+   res.render("projects.ejs",{projects});
+  });
+  }catch(err){
+    console.log(err);
+  }
+});
+
 app.listen(port ,()=>{
   console.log(`app is listening on port:${port}`);
 });
