3886345eda59ad6ea2fd05e4bcb414cfd24b9da6
Ranjan Kumar <imranjanyadav2001@gmail.com>
2026-08-22 14:49:12 +0530

    require new packages

--- index.js
+++ index.js
@@ -3,12 +3,29 @@ const app = express();
 const port = 8080;
 const path = require("path");
 
+require("dotenv").config();
+
+const mysql = require("mysql2");
+
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"/views"));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
 
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+db.connect((err) => {
+    if (err) {
+        console.log("Database connection failed:", err);
+        return;
+    }
 
-
+    console.log("MySQL connected!");
+});
 app.listen(port ,()=>{
   console.log(`app is listening on port:${port}`);
 });
