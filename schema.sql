7e99a99913bb7eb92a5a7464ca6fb96746c610a9
Ranjan Kumar <imranjanyadav2001@gmail.com>
2026-08-22 14:44:00 +0530

    create file and define database and table inside it

---
+++ schema.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE IF NOT EXISTS portfolio;
+
+USE portfolio;
+
+CREATE TABLE IF NOT EXISTS projects (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    description TEXT NOT NULL,
+    technologies VARCHAR(255),
+    github VARCHAR(255),
+    live_demo VARCHAR(255)
+);
