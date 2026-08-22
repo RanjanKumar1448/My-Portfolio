c1171e05951ea8b7ce8c5fe52e7b9ad5029d42f0
Ranjan Kumar <imranjanyadav2001@gmail.com>
2026-08-22 12:10:33 +0530

    Set up Express server and EJS

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+const path = require("path");
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"/views"));
+app.use(express.static(path.join(__dirname, "public")));
+
+
+
+app.listen(port ,()=>{
+  console.log(`app is listening on port:${port}`);
+});
